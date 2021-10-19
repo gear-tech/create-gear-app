48521fe26ea576b3f767925606378c9af9af00be
EugeneWay <relax211@gmail.com>
2021-10-19 14:37:05 +0100

    add: wasm-proc utility

--- contract/buildwasm.js
+++ contract/buildwasm.js
@@ -1,7 +1,7 @@
 const sh = require("shelljs");
 const path = require("path");
 
-const projectDir = sh.pwd().toString()
+const projectDir = sh.pwd().toString();
 
 // Move to script directory
 sh.cd(path.join(__dirname));
@@ -19,17 +19,24 @@ const { code } = sh.exec(buildCmd);
 // if success
 
 if (code === 0 && projectDir !== __dirname) {
-    const outDir = `${projectDir}/out`;
-    const packageName = require("fs")
-      .readFileSync(`${__dirname}/Cargo.toml`)
-      .toString()
-      .match(/name = "([^"]+)"/)[1];
-    const wasm = `${projectDir}/out/${packageName}.wasm`;
-    const outFile = `./target/wasm32-unknown-unknown/${debug ? "debug" : "release"}/${packageName}.wasm`;
-    sh.mkdir("-p", outDir);
-    sh.rm("-f", wasm);
-    sh.cp("-u", outFile, wasm);
+  const outDir = `${projectDir}/out`;
+  const packageName = require("fs")
+    .readFileSync(`${__dirname}/Cargo.toml`)
+    .toString()
+    .match(/name = "([^"]+)"/)[1];
+  const wasm = `${projectDir}/out/${packageName}.wasm`;
+  const outFile = `./target/wasm32-unknown-unknown/${debug ? "debug" : "release"}/${packageName}.wasm`;
+  sh.mkdir("-p", outDir);
+  sh.rm("-f", wasm);
+  sh.cp("-u", outFile, wasm);
+
+//   Check if Gear wasm-proc util already installed
+  if (sh.which('wasm-proc')) {
+    sh.echo('Sorry, wasm-proc is not installed. Installing...');
+    sh.exec('cargo install --git https://github.com/gear-tech/gear wasm-proc');
+  } else {
+    sh.exec(`wasm-proc --path=${wasm}`);
   }
-  
+}
 
 console.log(code);
