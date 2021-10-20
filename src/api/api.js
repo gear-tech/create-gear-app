5bb4f5eb4674f65320adfc5585eb4fc68d1a200d
EugeneWay <relax211@gmail.com>
2021-10-20 14:22:57 +0100

    add: register types

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,23 @@ import { GearApi } from '@gear-js/api';
 // Connecting to RPC node
 
 async function initApi() {
-  const gear = await GearApi.create();
+  const gear = await GearApi.create({
+    customTypes: {
+      Action: {
+        _enum: {
+          AddMessage: {
+            author: 'String',
+            msg: 'String',
+          },
+          ViewMessages: 'Null',
+        },
+      },
+      MessageIn: {
+        author: 'String',
+        msg: 'String',
+      },
+    },
+  });
   return gear;
 }
 
