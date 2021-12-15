9ced43610e6a633d921ff9a10f9acdddc8d086d2
EugeneWay <relax211@gmail.com>
2021-12-15 12:20:37 +0000

    fix: contract & parseTypes

--- contract/buildwasm.js
+++ contract/buildwasm.js
@@ -1,5 +1,7 @@
 const sh = require("shelljs");
 const path = require("path");
+const fs = require("fs");
+const { getWasmMetadata } = require("@gear-js/api");
 
 const projectDir = sh.pwd().toString();
 
@@ -10,8 +12,8 @@ const debug = process.argv.pop() === "--debug";
 
 // check out if we have 'debug' flag in options
 const buildCmd = debug
-  ? "cargo +nightly build --target wasm32-unknown-unknown"
-  : "cargo +nightly build --target wasm32-unknown-unknown --release";
+  ? "cargo +nightly build"
+  : "cargo +nightly build --release";
 
 // Run cargo build command and save exit code
 const { code } = sh.exec(buildCmd);
@@ -19,24 +21,22 @@ const { code } = sh.exec(buildCmd);
 // if success
 
 if (code === 0 && projectDir !== __dirname) {
-  const outDir = `${projectDir}/out`;
+  const outDir = `${projectDir}/public/out`;
   const packageName = require("fs")
     .readFileSync(`${__dirname}/Cargo.toml`)
     .toString()
     .match(/name = "([^"]+)"/)[1];
-  const wasm = `${projectDir}/out/${packageName}.wasm`;
+  const wasm = `${projectDir}/public/out/${packageName}.wasm`;
   const outFile = `./target/wasm32-unknown-unknown/${debug ? "debug" : "release"}/${packageName}.wasm`;
   sh.mkdir("-p", outDir);
   sh.rm("-f", wasm);
   sh.cp("-u", outFile, wasm);
 
-//   Check if Gear wasm-proc util already installed
-  if (!sh.which('wasm-proc')) {
-    sh.echo('Sorry, wasm-proc is not installed. Installing...');
-    sh.exec('cargo install --git https://github.com/gear-tech/gear wasm-proc');
+  //   Check if Gear wasm-proc util already installed
+  if (!sh.which("wasm-proc")) {
+    sh.echo("Sorry, wasm-proc is not installed. Installing...");
+    sh.exec("cargo install --git https://github.com/gear-tech/gear wasm-proc");
   } else {
-    sh.exec(`wasm-proc --path=${wasm}`);
+    sh.exec(`wasm-proc -p ${wasm}`);
   }
 }
-
-console.log(code);
