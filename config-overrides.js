b7e4d47f43f86ff062cc526f7832703ee4c97f8d
EugeneWay <relax211@gmail.com>
2021-12-24 17:23:59 +0300

    add: Buffer polifill

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,22 @@
+const webpack = require('webpack');
+
 module.exports = function override(config, env) {
   config.resolve.alias = {
     ...config.resolve.alias,
     'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
     'react/jsx-runtime': 'react/jsx-runtime.js',
   };
+
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    buffer: require.resolve('buffer/'),
+  };
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  );
 
   return config;
 };
