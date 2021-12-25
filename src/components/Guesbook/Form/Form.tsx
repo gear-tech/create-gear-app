cdf00d04b196350e03784e61ef0068c03623cef3
EugeneWay <relax211@gmail.com>
2021-12-25 14:47:53 +0300

    add: form

--- src/components/Guesbook/Form/Form.tsx
+++ src/components/Guesbook/Form/Form.tsx
@@ -1,41 +1,60 @@
 import React, { useState } from 'react';
-import './Form.scss'
+import './Form.scss';
 
 export const Form = () => {
   const [messageContext, setMessageContext] = useState<string>('');
   const [valueContext, setValueContext] = useState<number>(0);
 
   // Search submit example
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    console.log(messageContext)
-    console.log(valueContext)
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(messageContext);
+    console.log(valueContext);
   };
 
   return (
-    <>
-      <form className="message-form">
-          <label htmlFor="value" className="message-form__label">Message:</label>
+    <form className="message-form">
+      <input
+        id="message"
+        type="text"
+        placeholder="Type your message:"
+        value={messageContext}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          setMessageContext(event.target.value)
+        }
+        autoComplete="off"
+        required
+      />
+      <footer>
+        <div className="counter">
+          <div className="title">Value:</div>
+          <span
+            className="minus"
+            onClick={() => {
+              if (valueContext > 0) {
+                setValueContext(valueContext - 1);
+              }
+            }}
+          ></span>
           <input
-            type="text"
-            id="message"
-            className="message-form__message"
-            placeholder="Type your message:"
-            value={messageContext}
-            onChange={(e: any) => setMessageContext(e.target.value)}
-            autoComplete="off"
-            required
-          />
-          <label htmlFor="value"  className="message-form__label">Value:</label>
-          <input 
             type="number"
+            min="0"
+            step="0.1"
             className="message-form__value"
             id="value"
+            readOnly
             value={valueContext}
             onChange={(e: any) => setValueContext(e.target.value)}
           />
-      </form>
-      <button type="button" className="message-form__button success">Add Message</button>
-    </>
+          <span
+            className="plus"
+            onClick={() => setValueContext(valueContext + 1)}
+          ></span>
+        </div>
+        <button className="message-form__button success" onClick={handleSubmit}>
+          Add message
+        </button>
+      </footer>
+    </form>
   );
 };
