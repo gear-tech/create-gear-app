d249dc7f38109e350b821eb7ed17ba31657eb6bc
EugeneWay <relax211@gmail.com>
2022-01-12 12:41:41 +0300

    add: bump @gear-js -> 0.0.96 & crypto/stream polifill

--- config-overrides.js
+++ config-overrides.js
@@ -10,6 +10,8 @@ module.exports = function override(config, env) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
     buffer: require.resolve('buffer/'),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
   };
 
   config.plugins.push(
