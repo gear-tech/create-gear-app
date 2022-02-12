38ca0a419b8997c233a5eee4ec8fe0f93eae3636
codev0 <dakanca@gmail.com>
2022-02-12 18:34:14 +0600

    add arraybuffer-loader and change rewired to craco

---
+++ craco.config.js
@@ -0,0 +1,38 @@
+const webpack = require("webpack");
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
+        "react/jsx-runtime": "react/jsx-runtime.js",
+      };
+
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        buffer: require.resolve("buffer/"),
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+      };
+
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          Buffer: ["buffer", "Buffer"],
+        })
+      );
+
+      // webpackConfig.experiments = {
+      //   asyncWebAssembly: true,
+      // };
+
+      webpackConfig.module.rules.push({
+        test: /\.wasm$/,
+        type: "javascript/auto",
+        use: ["arraybuffer-loader"],
+      });
+
+      return webpackConfig;
+    },
+  },
+};
