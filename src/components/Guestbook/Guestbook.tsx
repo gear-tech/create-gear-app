3753f403af56a8f90b57c59aeab0351e9bb11f09
EugeneWay <relax211@gmail.com>
2022-02-27 14:45:44 +0000

    bump @gear-js -> 0.14.2

--- src/components/Guestbook/Guestbook.tsx
+++ src/components/Guestbook/Guestbook.tsx
@@ -22,6 +22,7 @@ export const Guestbook = () => {
     // TODO: add type
     const buffer = await import('../../out/guestbook.meta.wasm');
     const state = await api.programState.read(CONTRACT_ADDRESS, buffer.default);
+
     setMessages(state.toHuman() as Message[]);
   }, [api.programState]);
 
